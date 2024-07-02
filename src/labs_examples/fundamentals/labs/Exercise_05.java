d99c1fd272bc5695952df06fa624511c3d3f9122
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2024-07-02 17:01:29 +0200

    third exercise completed

--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,15 +14,27 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int b = str.length();
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
+        /* returns true if Strings match */
+        boolean equals = str.equals(str2);
+        System.out.println(equals);
 
         // please concatenate str & str2 and set the result to a new String variable below
+        String s = "String 1 and 2 is ... " + str + str2;
+        System.out.println(s);
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+        int search = "hello".indexOf("o");
 
+        boolean equalStrings = str.equalsIgnoreCase(str2);
+        System.out.println("Do str1 & str2 match? " + equalStrings);
+
+        String subStr = str.substring(8, 12);
+        System.out.println("The substring is: " + subStr);
     }
 
 
