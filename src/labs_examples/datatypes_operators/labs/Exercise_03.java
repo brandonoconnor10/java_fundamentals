feeb646c378694eac30ba2ea67e26bc27c77c08e
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2024-07-02 19:00:01 +0200

    Third Operators exercise completed

--- src/labs_examples/datatypes_operators/labs/Exercise_03.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_03.java
@@ -11,6 +11,18 @@ class ArithmeticOperators {
     public static void main(String[] args) {
 
         // write your code below
+        int x = 5;
+        int y = 6;
+        int z1 = x + y;
+        int z2 = x - y;
+        int z3 = x * y;
+        int z4 = y / x;
+        int z5 = x % y;
+        System.out.println(z1);
+        System.out.println(z2);
+        System.out.println(z3);
+        System.out.println(z4);
+        System.out.println(z5);
 
     }
 
