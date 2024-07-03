00a3b0f68b8b0b26014460f0651200caad00c8bd
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2024-07-03 15:10:42 +0200

    Conditional loops exercise completed

--- src/labs_examples/conditions_loops/labs/Exercise_09.java
+++ src/labs_examples/conditions_loops/labs/Exercise_09.java
@@ -8,4 +8,15 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_09 {
+    public static void main(String[] args) {
+        // Using a for loop to demonstrate the break statement
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                // Break statement to exit the loop when i equals 5
+                break;
+            }
+            System.out.println("i: " + i);
+        }
+        System.out.println("Loop exited because break was encountered.");
+    }
 }
