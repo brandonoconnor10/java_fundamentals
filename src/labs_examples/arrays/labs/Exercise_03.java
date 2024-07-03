cba96f3eeba7b991e5226d1d5746e61ddfd96c95
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2024-07-03 15:16:40 +0200

    Conditional loops exercise completed

--- src/labs_examples/arrays/labs/Exercise_03.java
+++ src/labs_examples/arrays/labs/Exercise_03.java
@@ -18,6 +18,24 @@ package labs_examples.arrays.labs;
 public class Exercise_03 {
 
     public static void main(String[] args) {
-        
+        // Create a 5x5 2D array
+        int[][] array = new int[5][5];
+
+        // Populate the array with multiples of 3
+        int startValue = 3;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = startValue;
+                startValue += 3; // Increment startValue by 3 for the next element
+            }
+        }
+
+        // Print the array
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println(); // Move to the next line after each row
+        }
     }
 }
