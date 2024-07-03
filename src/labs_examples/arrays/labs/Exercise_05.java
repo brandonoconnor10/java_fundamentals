aa76a6d1093114c286cc92473424d6436f572141
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2024-07-03 15:18:06 +0200

    Conditional loops exercise completed

--- src/labs_examples/arrays/labs/Exercise_05.java
+++ src/labs_examples/arrays/labs/Exercise_05.java
@@ -9,5 +9,14 @@ package labs_examples.arrays.labs;
  */
 
 public class Exercise_05 {
+    public static void main(String[] args) {
+        // Create and populate an array
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+        // Print every other element in reverse order
+        for (int i = array.length - 2; i >= 0; i -= 2) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println(); // Move to the next line after printing
+    }
 }
