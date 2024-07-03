26b08a69420923646f0cb1c3e0e2067040573d2d
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2024-07-03 15:11:41 +0200

    Conditional loops exercise completed

--- src/labs_examples/conditions_loops/labs/Exercise_10.java
+++ src/labs_examples/conditions_loops/labs/Exercise_10.java
@@ -8,4 +8,15 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_10 {
+    public static void main(String[] args) {
+        // Using a for loop to demonstrate the continue statement
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                // Continue statement to skip the current iteration when i equals 5
+                continue;
+            }
+            System.out.println("i: " + i);
+        }
+        System.out.println("Loop completed, skipping number 5.");
+    }
 }
