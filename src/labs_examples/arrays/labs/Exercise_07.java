0c3317e7a2f902bfdddb3a88b300740e1bd9f7ae
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2024-07-03 15:21:35 +0200

    exercise completed

--- src/labs_examples/arrays/labs/Exercise_07.java
+++ src/labs_examples/arrays/labs/Exercise_07.java
@@ -1,4 +1,6 @@
 package labs_examples.arrays.labs;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 /**
  *  ArrayLists
@@ -10,4 +12,34 @@ package labs_examples.arrays.labs;
  *
  */
 public class Exercise_07 {
+    public static void main(String[] args) {
+        // Create an ArrayList of integers
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        // Populate the ArrayList
+        arrayList.add(10);
+        arrayList.add(20);
+        arrayList.add(30);
+        arrayList.add(40);
+        arrayList.add(50);
+
+        // Access elements within the ArrayList
+        System.out.println("Elements in the ArrayList:");
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.println("Index " + i + ": " + arrayList.get(i));
+        }
+
+        // Explore ArrayList methods
+        System.out.println("\nMethods available for ArrayList:");
+        System.out.println("-------------------------------");
+        for (Method method : arrayListMethods()) {
+            System.out.println(method);
+        }
+    }
+
+    // Method to list available methods for ArrayList
+    public static Method[] arrayListMethods() {
+        ArrayList<Integer> temp = new ArrayList<>();
+        return temp.getClass().getMethods();
+    }
 }
