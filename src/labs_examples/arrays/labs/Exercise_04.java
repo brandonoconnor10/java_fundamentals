93870e1e68a59dfee84a2e4568aece09be34bcee
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2024-07-03 15:17:23 +0200

    Conditional loops exercise completed

--- src/labs_examples/arrays/labs/Exercise_04.java
+++ src/labs_examples/arrays/labs/Exercise_04.java
@@ -9,5 +9,22 @@ package labs_examples.arrays.labs;
  */
 
 public class Exercise_04 {
+    public static void main(String[] args) {
+        // Create and populate an irregular 2D array
+        int[][] irregularArray = {
+                {1, 2, 3},
+                {4, 5},
+                {6, 7, 8, 9},
+                {10},
+                {11, 12, 13}
+        };
 
+        // Iterate and print each element using nested for-each loop
+        for (int[] row : irregularArray) {
+            for (int element : row) {
+                System.out.print(element + " ");
+            }
+            System.out.println(); // Move to the next line after each row
+        }
+    }
 }
